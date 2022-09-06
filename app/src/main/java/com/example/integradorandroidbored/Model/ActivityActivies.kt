62d21f4b40ee6d4e6eeadaf8570019ecc5952c0b
Debora Tolaba <debora.tolaba@mercadolibre.com>
2022-09-06 17:14:47 -0300

    ListView

--- app/src/main/java/com/example/integradorandroidbored/Model/ActivityActivies.kt
+++ app/src/main/java/com/example/integradorandroidbored/Model/ActivityActivies.kt
@@ -1,12 +1,30 @@
 package com.example.integradorandroidbored.Model
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.integradorandroidbored.R
+import com.example.integradorandroidbored.databinding.ActivityActiviesBinding
 
 class ActivityActivies : AppCompatActivity() {
+    private lateinit var binding: ActivityActiviesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_activies)
+
+        binding = ActivityActiviesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val list = arrayOf("Education", "Recreational", "Social", "Diy", "Charity", "Cooking", "Relaxation", "Music", "Busywork")
+        val arrayAdapter = ArrayAdapter<String>(this, R.layout.item, list)
+
+        binding.listView.adapter = arrayAdapter
+        binding.listView.setOnItemClickListener{ parent, view, position, id ->
+            val element = arrayAdapter.getItem(position)
+            Toast.makeText(this, element, Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
